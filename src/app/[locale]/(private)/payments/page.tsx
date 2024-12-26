eb775977f3456e42c1035c4e9c53863267f657e5
Minh Nguyen <anhminh1782002@gmail.com>
2024-12-26 15:34:59 +0700

    fix: build

--- src/app/[locale]/(private)/payments/page.tsx
+++ src/app/[locale]/(private)/payments/page.tsx
@@ -1,19 +1,3 @@
-import { PostView } from "@/components/common";
-import { usePayments } from "./logic";
-import { useTranslations } from "next-intl";
-
 export default function PaymentPage() {
-  const { columns } = usePayments();
-  const t = useTranslations();
-
-  return (
-    <PostView
-      columns={columns}
-      data={[]}
-      showAdd
-      showSearch
-      showRefresh
-      breadcrumb={[{ title: t("common.payments"), url: "/payments" }]}
-    />
-  );
+  return <div>Payment</div>;
 }
--- src/schemas/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./signup.schema";
-export * from "./category.schema";
